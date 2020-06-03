0ab7ee4fb5a77e16b11aeca0c532e05f6ff33f83
ellie <ellie.tms1371@gmail.com>
2020-06-03 21:45:27 +0100

    Update Jobs.js

--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -24,10 +24,10 @@ const Jobs = ({job : {
     return(
   <div className={`flex flex-col bg-white shadow-md my-16 mx-10 p-6 rounded
    ${featured && 'border-l-4 border-teal-500 border-solid'}
-  } md:flex-row`}>
+  } sm:flex-row`}>
     <div>
       <img className="-mt-16 mb-4 w-20 h-20
-       md:my-0  sm:h-24 sm:w-24"
+       sm:my-0  sm:h-24 sm:w-24"
        src={logo} alt={company} />
     </div>
     <div className="flex flex-col 
@@ -54,9 +54,9 @@ const Jobs = ({job : {
         {postedAt} . {contract} . {location}
       </p>
     </div>
-    <div className="flex flex-wrap 
-     items-center mt-4 pt-4 border-t border-gray-200 
-     border-solid sm:ml-auto md:border-0">
+    <div className="flex flex-wrap
+     items-center mt-4 pt-4 border-t  border-gray-200 
+     border-solid sm:ml-auto sm:border-0">
 
         {langsAndTools.map((langAndTool) =>
                 <span className="text-teal-500 mb-2 bg-teal-100 font-bold
