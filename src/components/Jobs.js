b2c45c2532282dc602af1a4a0013cb8e57e5e3b4
ellie <ellie.tms1371@gmail.com>
2020-06-03 21:42:41 +0100

    add css

--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -24,24 +24,29 @@ const Jobs = ({job : {
     return(
   <div className={`flex flex-col bg-white shadow-md my-16 mx-10 p-6 rounded
    ${featured && 'border-l-4 border-teal-500 border-solid'}
-  }`}>
+  } md:flex-row`}>
     <div>
-      <img className="-mt-16 mb-4 w-20 h-20" src={logo} alt={company} />
+      <img className="-mt-16 mb-4 w-20 h-20
+       md:my-0  sm:h-24 sm:w-24"
+       src={logo} alt={company} />
     </div>
-    <div className="flex flex-col justify-between  ml-4">
+    <div className="flex flex-col 
+     ml-4 ">
       <h3 className="font-bold  text-teal-500 ">
           {company}
           {isNew && (
           <span className="text-teal-100 
-          bg-teal-500 font-bold m-2 p-2 rounded-full">
-              New
+          bg-teal-500 font-bold
+          uppercase mx-2 p-2 rounded-full">
+              New!
          </span>)
           }   
           {featured && (
           <span  className="text-white
-          bg-red-700 font-bold m-2 px-2 py-1 rounded-full
-          ">
-          Featured</span>)
+          bg-red-700 font-bold m-2
+          uppercase px-2 py-1 rounded-full">
+          Featured
+          </span>)
           }         
       </h3>
       <h2 className="font-bold text-2lg my-2">{position}</h2>
@@ -49,11 +54,13 @@ const Jobs = ({job : {
         {postedAt} . {contract} . {location}
       </p>
     </div>
-    <div className="flex flex-wrap mr-4  items-center mt-4 pt-4 border-t border-gray-200 border-solid">
+    <div className="flex flex-wrap 
+     items-center mt-4 pt-4 border-t border-gray-200 
+     border-solid sm:ml-auto md:border-0">
 
         {langsAndTools.map((langAndTool) =>
                 <span className="text-teal-500 mb-2 bg-teal-100 font-bold
-                 mr-2 p-2 rounded">
+                 mr-2 py-2 rounded ">
                     {langAndTool}
                 </span>)
         }
