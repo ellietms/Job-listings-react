c788545c1e8ff0d3d66d9dc20305b42a0148d5ea
ellie <ellie.tms1371@gmail.com>
2020-06-03 20:03:05 +0100

    add codes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState, useEffect} from 'react';
+import JobBoard from './components/Jobs'; 
+import data from './data.json';
 import './App.css';
 
+
+
 function App() {
+  const [jobs,setJobs] = useState([]);
+  useEffect(() => {setJobs(data)},[]);
+  console.log(jobs);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {jobs.length === 0 ? (
+           <p>Jobs are fetching...</p>
+         ) : (
+           jobs.map(eachJob => (
+             <JobBoard job={eachJob} key={eachJob.id} />
+           ))
+         )
+      }
     </div>
   );
 }
---
+++ src/components/Jobs.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const Jobs = ({job : {
+   company,
+    logo,
+    isNew,
+    featured,
+    position,
+    role,
+    level,
+    postedAt,
+    contract,
+    location,
+    languages,
+    tools }
+}) => (
+  <div className="flex bg-white shadow-md m-4 p-4">
+    <div>
+      <img src={logo} alt={company} />
+    </div>
+    <div className="flex flex-col justify-between  ml-4">
+      <h3 className="font-bold  text-teal-500 ">{company}</h3>
+      <h2 className="font-bold text-2lg">{position}</h2>
+      <p className="text-gray-700">
+        {postedAt} . {contract} . {location}
+      </p>
+    </div>
+    <div></div>
+  </div>
+);
+
+export default Jobs;
